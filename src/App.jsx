1ae64e18d6d4a46c49928490d9da97c54058b1a0
Vlad <belousov7vlad@gmail.com>
2022-11-08 18:21:14 +0300

    router

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import './styles/global.sass';
-
-function App() {
-  return (
-    <div className="App">Особеный тип</div>
-  );
-}
-
-export default App;
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import '../../styles/global.sass';
+
+function MainPage() {
+  return (
+    <div className="main">
+главная
+    </div>
+  );
+}
+
+export default MainPage;
