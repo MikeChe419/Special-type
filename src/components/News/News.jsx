578ebab16b176b8b63ee68022e0f12c65beed09f
Vlad <belousov7vlad@gmail.com>
2022-11-10 19:17:44 +0300

    News add

---
+++ src/components/News/News.jsx
@@ -0,0 +1,9 @@
+import './News.sass'
+
+
+const News = () => {
+
+  return('news')
+}
+
+export default News
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,6 +3,7 @@ import About from '../../components/About/About';
 import Header from '../../components/Header/Header'
 import NavigationBlock from '../../components/NavigationBlock/NavigationBlock';
 import Footer from '../../components/Footer/Footer'
+import News from '../../components/News/News';
 
 
 const Main = () => {
@@ -10,6 +11,7 @@ const Main = () => {
     <div className="main">
       <Header />
       <About />
+      <News />
       <NavigationBlock />
       <Footer />
     </div>
