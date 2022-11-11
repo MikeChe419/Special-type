268e7ad5c2419e0b51341b5c2f7abdb48d05fab6
Ruslan Gaziev <grinrus@yandex.ru>
2022-11-11 15:22:50 +0300

    header, footer, navigation-block transfered to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './styles/@global.sass';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Header from './components/Header/Header'
+import NavigationBlock from './components/NavigationBlock/NavigationBlock';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route element={<Main />} path='/' />
       </Routes>
+      <NavigationBlock />
+      <Footer />
     </div>
   );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,5 @@
 import './Main.sass';
 import About from '../../components/About/About';
-import Header from '../../components/Header/Header'
-import NavigationBlock from '../../components/NavigationBlock/NavigationBlock';
-import Footer from '../../components/Footer/Footer'
 import News from '../../components/News/News';
 import Reviews from '../../components/Reviews/Reviews';
 
@@ -10,12 +7,9 @@ import Reviews from '../../components/Reviews/Reviews';
 const Main = () => {
   return (
     <div className="main">
-      <Header />
       <About />
       <News />
       <Reviews />
-      <NavigationBlock />
-      <Footer />
     </div>
   )
 }
