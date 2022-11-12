49ae55b528edcc131beac26031eb68041c482101
Ruslan Gaziev <grinrus@yandex.ru>
2022-11-13 00:31:00 +0300

    reviews error key fixed

--- src/pages/AllNews/AllNews.jsx
+++ src/pages/AllNews/AllNews.jsx
@@ -1,16 +1,15 @@
-import './AllNews.sass'
-import NewsCard from '../../components/NewsCard/NewsCard';
+import "./AllNews.sass";
+import NewsCard from "../../components/NewsCard/NewsCard";
 
 const AllNews = () => {
-
   return (
-    <section className='AllNews'>
-    <h2 className='AllNews__title'>НОВОСТИ</h2>
-    <div className="AllNews__content">
-    <NewsCard />
-    </div>
+    <section className="AllNews">
+      <h2 className="AllNews__title">НОВОСТИ</h2>
+      <div className="AllNews__content">
+        <NewsCard />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default AllNews
+export default AllNews;
