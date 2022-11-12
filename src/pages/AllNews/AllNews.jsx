c9d2e6fd36c2d1f4916d7b48b0c740ab33c9f506
Vlad <belousov7vlad@gmail.com>
2022-11-12 16:51:49 +0300

    news page

--- src/pages/AllNews/AllNews.jsx
+++ src/pages/AllNews/AllNews.jsx
@@ -1,9 +1,26 @@
 import './AllNews.sass'
+import  dataNews  from '../../TEMP_NEWS'
 
 
 const AllNews = () => {
 
-return('')
+
+
+  return (
+    <div className="AllNews__content">
+
+      {dataNews.map(news => (<div className="AllNews-card" key={news.id}>
+        <p className='news-card__create-date'>{news.createDate}</p>
+        <h3 className='news-card__title'>{news.name}</h3>
+        <p className='news-card__text'>{news.text}</p>
+        <button className='news-card__button'>читать дальше </button>
+        <img className='news-card__img' alt=''></img>
+      </div>))}
+
+
+
+    </div>
+  )
 }
 
 export default AllNews
