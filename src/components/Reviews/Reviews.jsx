fd6237be57cb9644aa7f4f02eecc6c1d369c51e9
Ruslan Gaziev <grinrus@yandex.ru>
2022-11-13 23:58:06 +0300

    uneversal mask

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,6 @@
+import getMask from '../../utils/getMask/getMask';
 import './Reviews.sass';
-import reviewImgOne from './review-img-1.png';
-import reviewImgTwo from './review-img-2.png';
-import reviewImgThree from './review-img-3.png';
+import tempFoto from './tempFoto.jpeg'
 
 const Reviews = () => {
 
@@ -11,21 +10,21 @@ const Reviews = () => {
       date: '08.10.2022',
       user: 'Елена Романова',
       review: 'Оля,огромное вам спасибо! Для Ромы это очень ВАЖНО!...',
-      image: reviewImgOne,
+      image: tempFoto,
       id: 1,
     },
     {
       date: '27.09.2022',
       user: 'Вероника Кудрявцева',
       review: 'После прочтения захотелось укутаться в плед, заварить тёплый чай и смотреть на то, как падает листва. Очень по-уютному. Вы молодцы, ребята!...',
-      image: reviewImgTwo,
+      image: tempFoto,
       id: 2,
     },
     {
       date: '12.09.2022',
       user: 'Данила Тостиашвили',
       review: 'Мне панравились с вами работать паэтому я ещё с вами надолго останусь вы талантливы и крутые ребята вы мне стали как радные люди...',
-      image: reviewImgThree,
+      image: tempFoto,
       id: 3,
     }
   ]
@@ -42,7 +41,13 @@ const Reviews = () => {
               <h3 className='reviews__card-title'>{item.user}</h3>
               <p className='reviews__text'>{item.review}</p>
               <button className='reviews__button'>читать дальше </button>
-              <img className='reviews__card-img' src={item.image} alt={item.user}></img>
+              <div className='reviews__image-container'>
+                <img className='reviews__card-img' src={item.image} alt={item.user} />
+              </div>
+
+              {
+                getMask('review')
+              }
             </div>
           ))
         }
