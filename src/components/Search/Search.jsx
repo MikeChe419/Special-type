b82a95f793a5742c0bd157160c5ea2fe367cec0d
Vlad <belousov7vlad@gmail.com>
2022-11-22 20:28:02 +0300

    search component

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,39 @@
+import './Search.sass'
+
+import React from 'react'
+export const Search = () => {
+
+  const [isSearchOpen, setIsSearchOpen] = React.useState(false)
+  const [searchInput, setSearchInput] = React.useState('')
+
+
+  const handleOpenSearch = () => {
+    setIsSearchOpen(true)
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault()
+    alert('запрос данных')
+    ///запрос данных с апи или сторедж
+  }
+
+  const handleChangeValue = (event) => {
+    const search = event.target.value.toLowerCase();
+    setSearchInput(search);
+  }
+
+  function submitForm(event) {
+    event.preventDefault()
+  }
+
+  return (
+    <div className="search" >
+      <form className={isSearchOpen ? "search__form" : 'search__form_type_hidden'} onSubmit={submitForm}>
+        <input type="text" className="search__input" onChange={handleChangeValue} placeholder="Найти..." />
+      </form>
+      <button className={isSearchOpen ? "search__button" : 'search__button search__button_type_hidden'} onClick={isSearchOpen ? handleSearch : handleOpenSearch}></button>
+    </div>
+  )
+
+}
+
