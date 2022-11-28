48c9c118f914876d7deac180ff3e3ce2c87a759d
Ruslan Gaziev <grinrus@yandex.ru>
2022-11-28 09:52:35 +0300

    registration form ankor changed added

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -7,11 +7,21 @@ const Registration = ({ itemForRegistration }) => {
   const handleOnClick = () => {
     navigate(-1);
   }
+
+  const createProperAnkor = () => {
+    if (itemForRegistration.price) {
+      return 'Афиша / Мероприятие / Регистрация'
+    };
+    return 'Расписание занятий и репетиций / Репетиция / Регистрация'
+  }
+
+  const ankor = createProperAnkor()
+
   return (
     <section className='registration'>
       <nav className='registration__go-back' onClick={handleOnClick}>
-        <div className='registration__arrow'></div>
-        <p className='registration__text'>Афиша / Мероприятие / Регистрация</p>
+        <div className='registration__arrow' />
+        <p className='registration__text'>{ ankor }</p>
       </nav>
       <Form itemForRegistration={itemForRegistration} />
     </section>
