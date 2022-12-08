aa76408725ee19b62f35d5eb4aec95858434ceba
Vlad <belousov7vlad@gmail.com>
2022-12-08 20:38:04 +0300

    GoD link and some styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,48 +1,78 @@
-import './Footer.sass'
+import "./Footer.sass";
 const Footer = () => {
   return (
-    <footer className='footer'>
+    <footer className="footer">
       <div className="footer__info-block">
-        <div className="footer__logo"></div>
         <div className="footer__first-block">
-          <p className="footer__first-block-title">«Особенный ТИП»</p>
-          <p className="footer__first-block-subtitle">Первый инклюзивный театральный проект в Новосибирске</p>
-          <ul className="footer__first-block-icons">
-            <li className="footer__info-block-element">
-              <a href='https://vk.com/osobenny_tip_nsk' target='_blank' rel="noreferrer">
-                <div className="footer__first-block-vk"/>
-              </a>
-            </li>
-            <li className="footer__info-block-element">
-              <a href='https://t.me/osobenniy_tip' target='_blank' rel="noreferrer">
-                <div className="footer__first-block-tm"/>
-              </a>
-            </li>
-            <li className="footer__info-block-element">
-              <a href='https://wa.me/79612188417' target='_blank' rel="noreferrer">
-              <div className="footer__first-block-ws"/>
-              </a>
-            </li>
-          </ul>
+          <div className="footer__logo"></div>
+          <div className="footer__first-block-content">
+            <p className="footer__first-block-title">«Особенный ТИП»</p>
+            <p className="footer__first-block-subtitle">
+              Первый инклюзивный театральный проект в Новосибирске
+            </p>
+            <ul className="footer__first-block-icons">
+              <li className="footer__info-block-element">
+                <a
+                  href="https://vk.com/osobenny_tip_nsk"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="footer__first-block-vk" />
+                </a>
+              </li>
+              <li className="footer__info-block-element">
+                <a
+                  href="https://t.me/osobenniy_tip"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="footer__first-block-tm" />
+                </a>
+              </li>
+              <li className="footer__info-block-element">
+                <a
+                  href="https://wa.me/79612188417"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="footer__first-block-ws" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
         <ul className="footer__second-block">
           <li className="footer__info-block-element">
-            <a className='footer__second-block-item' href="https://yandex.ru/maps/-/CCUbMNAJ0D" target='_blank' rel="noreferrer">
-              <div className='footer__second-block-address-icon'></div>
-              <address className='footer__second-block-address-text'>Россия, 633009, Новосибирская область, <br /> г. Бердск, ул. Боровая, 111А, медицинский центр "Святитель Лука"</address>
+            <a
+              className="footer__second-block-item"
+              href="https://yandex.ru/maps/-/CCUbMNAJ0D"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="footer__second-block-address-icon"></div>
+              <address className="footer__second-block-address-text">
+                Россия, 633009, Новосибирская область, <br /> г. Бердск, ул.
+                Боровая, 111А, медицинский центр "Святитель Лука"
+              </address>
             </a>
           </li>
           <li className="footer__info-block-element">
-            <a className='footer__second-block-item' href="tel:+79612188417">
-              <div className='footer__second-block-phone-icon'></div>
-              <p className='footer__second-block-phone-text'>+7 961 218 84 17</p>
+            <a className="footer__second-block-item" href="tel:+79612188417">
+              <div className="footer__second-block-phone-icon"></div>
+              <p className="footer__second-block-phone-text">
+                +7 961 218 84 17
+              </p>
             </a>
-
           </li>
           <li className="footer__info-block-element">
-            <a href="mailto:ostvolova@mail.ru" className="footer__second-block-item">
-              <div className='footer__second-block-mail-icon'></div>
-              <p className='footer__second-block-mail-text'>ostvolova@mail.ru</p>
+            <a
+              href="mailto:ostvolova@mail.ru"
+              className="footer__second-block-item"
+            >
+              <div className="footer__second-block-mail-icon"></div>
+              <p className="footer__second-block-mail-text">
+                ostvolova@mail.ru
+              </p>
             </a>
           </li>
         </ul>
@@ -50,9 +80,10 @@ const Footer = () => {
       <div className="footer__law-block">
         <p className="footer__law-block-text">{`© ${new Date().getFullYear()}, Особенный ТИП, все права защищены`}</p>
         <p className="footer__law-block-text">Политика конфиденциальности</p>
+        <a className="footer__law-block-link" href="http://guild-of-developers.ru/" target="_blank">Разработка сайта </a>
       </div>
     </footer>
   )
 }
 
-export default Footer;
+export default Footer
