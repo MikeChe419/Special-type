c2354d7d69aad9e2dd8e766492c2d47a35eebce7
Ruslan Gaziev <grinrus@yandex.ru>
2022-12-11 21:39:33 +0300

    crash on single review page was fixed

--- src/pages/ReviewSingle/ReviewSingle.jsx
+++ src/pages/ReviewSingle/ReviewSingle.jsx
@@ -2,32 +2,36 @@ import "./ReviewSingle.sass";
 import { useParams } from "react-router-dom";
 import getMask from "../../utils/getMask/getMask";
 import { GoBackButton } from "../../components/GoBackButton/GoBackButton";
-import ImageMasked from '../../components/ImageMasked/ImageMasked';
+import ImageMasked from "../../components/ImageMasked/ImageMasked";
 
-const ReviewSingle = ({dataReviews, handleClickOpenModal}) => {
+const ReviewSingle = ({ dataReviews, handleClickOpenModal }) => {
   const { id } = useParams();
   const review = dataReviews.find((item) => item.id == id);
 
   const onClick = () => {
-    handleClickOpenModal(review)
-  }
+    handleClickOpenModal(review);
+  };
 
   return (
     <>
       <GoBackButton label="Отзывы / Подробный отзыв" />
       <section className="review-single">
-        <div className="review-single__block">
-          <div className="review-single__review-block">
-            <h2 className="review-single__date">{review.date ? review.date.slice(0, 10).replace(/-/g, ".") : ''}</h2>
-            <h3 className="review-single__user">
-              {review.name.toLocaleUpperCase()}
-            </h3>
-            <p className="review-single__text">{review.description}</p>
+        {review && (
+          <div className="review-single__block">
+            <div className="review-single__review-block">
+              <h2 className="review-single__date">
+                {review.date ? review.date.slice(0, 10).replace(/-/g, ".") : ""}
+              </h2>
+              <h3 className="review-single__user">
+                {review.name.toLocaleUpperCase()}
+              </h3>
+              <p className="review-single__text">{review.description}</p>
+            </div>
+            <div className="review-single__image-container" onClick={onClick}>
+              <ImageMasked item={review} />
+            </div>
           </div>
-          <div className="review-single__image-container" onClick={onClick}>
-            <ImageMasked item={review} />
-          </div>
-        </div>
+        )}
         {getMask("review")}
       </section>
     </>
