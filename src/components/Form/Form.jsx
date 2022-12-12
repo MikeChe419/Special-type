3356d73764aaadd10b9024514ac359248dce16c2
Ruslan Gaziev <grinrus@yandex.ru>
2022-12-12 23:12:55 +0300

    sending form to email done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,13 @@
 import "./Form.sass";
 import { useForm } from "react-hook-form";
 import { nameRegExp, descriptionRegExp, emailRegExp, phoneRegExp } from '../../utils/regExp';
+import { sendEmail } from '../../utils/api/emailJSApi';
+import { useNavigate } from 'react-router-dom';
+import { YOUR_PUBLIC_KEY, YOUR_TEMPLATE_ID, YOUR_SERVICE_ID } from '../../utils/emailJS';
 
 const Form = ({ itemForRegistration }) => {
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
@@ -26,8 +31,6 @@ const Form = ({ itemForRegistration }) => {
     //  }
   };
 
-  console.log(errors);
-
   const subtitle = changeEnding();
 
   const onSubmit = (data) => {
@@ -35,7 +38,19 @@ const Form = ({ itemForRegistration }) => {
       ...data,
       ...itemForRegistration,
     };
-    alert(JSON.stringify(registrationData));
+
+    const dataToSend = {
+      service_id: YOUR_SERVICE_ID,
+      template_id: YOUR_TEMPLATE_ID,
+      user_id: YOUR_PUBLIC_KEY,
+      template_params: {
+          'message': Object.values(registrationData).join(' '),
+      }
+    };
+
+    sendEmail(dataToSend)
+    .then(() => navigate('/'))
+    .catch(err => console.log(err))
   };
 
   return (
---
+++ src/utils/emailJS.js
@@ -0,0 +1,3 @@
+export const YOUR_SERVICE_ID = 'service_p4lc32c'
+export const YOUR_TEMPLATE_ID = 'template_8pgugbu'
+export const YOUR_PUBLIC_KEY = 'qfrCAGG4-VA7nXFT5'
