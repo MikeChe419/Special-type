5a53bca25489a8989bbdb9c469ae3f268c91d9df
Vlad <belousov7vlad@gmail.com>
2022-12-12 15:21:27 +0300

    data cards count == width size

--- src/pages/AllCards/AllCards.jsx
+++ src/pages/AllCards/AllCards.jsx
@@ -8,6 +8,7 @@ import Pagination from "../../components//Pagination/Pagination";
 import ScheduleItem from "../../components/ScheduleItem/ScheduleItem";
 import { FriendsList } from "../../components/FriendsList/FriendsList";
 import Review from "../../components/Review/Review";
+import useMediaQuery from "../../utils/hooks/useMediaQuery";
 
 export const AllCards = ({
   cardsData,
@@ -17,17 +18,28 @@ export const AllCards = ({
   search,
 }) => {
   let location = useLocation();
-
+  const [dataCount, setDataCount] = useState(10);
   const [currentEventsPage, setCurrentEventPage] = useState([]);
   const [currentEvent, setCurrentEvent] = useState(1);
+
+  const isTablet = useMediaQuery("(max-width: 1000px)");
+  const isMobile = useMediaQuery("(max-width: 500px)");
+  useEffect(() => {
+    if (isMobile) {
+      setDataCount(4);
+    } else if (isTablet) {
+      setDataCount(6);
+    } else setDataCount(10);
+  }, [isMobile, isTablet]);
+
   useEffect(() => {
     if (cardsData.length !== undefined) {
-      let lastEventIndex = currentEvent * 10;
-      let firstEventIndex = lastEventIndex - 10;
+      let lastEventIndex = currentEvent * dataCount;
+      let firstEventIndex = lastEventIndex -  dataCount;
       let pages = cardsData.slice(firstEventIndex, lastEventIndex);
       setCurrentEventPage(pages);
     }
-  }, [cardsData, currentEvent]);
+  }, [cardsData, currentEvent, dataCount]);
 
   return (
     <section className="allCards">
