ffa69b290096a04381109b8c3b2ce032784075fd
Vlad <belousov7vlad@gmail.com>
2022-12-13 13:55:43 +0300

    fix

--- src/components/ImageMasked/ImageMasked.jsx
+++ src/components/ImageMasked/ImageMasked.jsx
@@ -8,6 +8,7 @@ const ImageMasked = ({ item, lable }) => {
 
   //ВРЕМЕННО. Подстановка порта в ЮРЛ
   const itemImg = () => {
+    console.log(item)
     let newImg = item.images[0].image.slice(22, 2000);
     let newImgUrl = `http://135.181.198.180:8080${newImg}`;
     return newImgUrl;
@@ -25,24 +26,28 @@ const ImageMasked = ({ item, lable }) => {
   // Генерация картинки заглушки для каждой отдельной страницы
   const plugImg = getPlug(lable);
 
+  console.log(item.images)
+
   return (
     <>
       <div className="image-masked__container">
-        {item.images.length > 0 ? (
+        {/* {Array.isArray(item.images) ? (
           <img
             className="image-masked__img"
             src={itemImg()}
             alt={item.user}
             style={{ clipPath: `url(#${generateMaskId(item.id)})` }}
           />
-        ) : (
+        ) :  */}
+        (
           <img
             className="image-masked__img"
             src={item.image ? item.image : plugImg}
             alt={item.user}
             style={{ clipPath: `url(#${generateMaskId(item.id)})` }}
           />
-        )}
+        )
+        {/* } */}
       </div>
       {getMask(generateMaskId(item.id))}
     </>
