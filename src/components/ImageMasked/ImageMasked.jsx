e6f178d3f2c2a745b3d1855477d56666dd76d5cd
Vlad <belousov7vlad@gmail.com>
2022-12-13 14:39:44 +0300

    fix

--- src/components/ImageMasked/ImageMasked.jsx
+++ src/components/ImageMasked/ImageMasked.jsx
@@ -5,18 +5,19 @@ import { getPlug } from "../../utils/getPlug";
 
 // Принимает объект: новость, отзыв и так далее, внутри которого есть id, image и name
 const ImageMasked = ({ item, lable }) => {
-
   //ВРЕМЕННО. Подстановка порта в ЮРЛ
   const itemImg = () => {
-    console.log(item)
-    let newImg = item.images[0].image.slice(22, 2000);
-    let newImgUrl = `http://135.181.198.180:8080${newImg}`;
-    return newImgUrl;
+    if (item.images.length == 0) {
+      return plugImg;
+    } else {
+      let newImg = item.images[0].image.slice(22, 2000);
+      let newImgUrl = `http://135.181.198.180:8080${newImg}`;
+      return newImgUrl;
+    }
   };
 
   //ВРЕМЕННО. Подстановка порта в ЮРЛ
 
-  
   // Генерация уникального id для маски. Если у объектов будет уникальный id, эту функцию нужно убрать
   const generateMaskId = (id) => {
     const maskId = `mask-${id}`;
@@ -26,28 +27,26 @@ const ImageMasked = ({ item, lable }) => {
   // Генерация картинки заглушки для каждой отдельной страницы
   const plugImg = getPlug(lable);
 
-  console.log(item.images)
+  // console.log(item.images.image)
 
   return (
     <>
       <div className="image-masked__container">
-        {/* {Array.isArray(item.images) ? (
+        {Array.isArray(item.images) ? (
           <img
             className="image-masked__img"
             src={itemImg()}
             alt={item.user}
             style={{ clipPath: `url(#${generateMaskId(item.id)})` }}
           />
-        ) :  */}
-        (
+        ) : (
           <img
             className="image-masked__img"
             src={item.image ? item.image : plugImg}
             alt={item.user}
             style={{ clipPath: `url(#${generateMaskId(item.id)})` }}
           />
-        )
-        {/* } */}
+        )}
       </div>
       {getMask(generateMaskId(item.id))}
     </>
