aee3defc892b230044dce92755141b71ea08ef3e
Vlad <belousov7vlad@gmail.com>
2022-12-14 01:44:30 +0300

    mediaqueries for singlenews/reviews

--- src/pages/ReviewSingle/ReviewSingle.jsx
+++ src/pages/ReviewSingle/ReviewSingle.jsx
@@ -4,10 +4,12 @@ import getMask from "../../utils/getMask/getMask";
 import { GoBackButton } from "../../components/GoBackButton/GoBackButton";
 import ImageMasked from "../../components/ImageMasked/ImageMasked";
 import { NotFound } from "../../components/NotFound/NotFound";
+import useMediaQuery from "../../utils/hooks/useMediaQuery";
 
 const ReviewSingle = ({ dataReviews, handleClickOpenModal }) => {
   const { id } = useParams();
   const review = dataReviews.find((item) => item.id == id);
+  const isMobile = useMediaQuery("(max-width: 500px)");
 
   const onClick = () => {
     handleClickOpenModal(review);
@@ -20,19 +22,29 @@ const ReviewSingle = ({ dataReviews, handleClickOpenModal }) => {
         {review ? (
           <div className="review-single__block">
             <div className="review-single__review-block">
-              <h2 className="review-single__date">
-                {review.date ? review.date.slice(0, 10).replace(/-/g, ".") : ""}
-              </h2>
-              <h3 className="review-single__user">
-                {review.name.toLocaleUpperCase()}
-              </h3>
+              {!isMobile ? (
+                <>
+                  {" "}
+                  <h2 className="review-single__date">
+                    {review.date
+                      ? review.date.slice(0, 10).replace(/-/g, ".")
+                      : ""}
+                  </h2>
+                  <h1 className="review-single__user">
+                    {review.name.toLocaleUpperCase()}
+                  </h1>
+                </>
+              ) : (
+                ""
+              )}
+
               <p className="review-single__text">{review.description}</p>
             </div>
             <div className="review-single__image-container" onClick={onClick}>
               <ImageMasked item={review} />
             </div>
           </div>
-        ):(
+        ) : (
           <NotFound />
         )}
         {getMask("review")}
--- src/pages/SingleNews/SingleNews.jsx
+++ src/pages/SingleNews/SingleNews.jsx
@@ -3,11 +3,13 @@ import "./SingleNews.sass";
 import { GoBackButton } from "../../components/GoBackButton/GoBackButton";
 import plugNews from "../../assets/images/plugs/plugNews.png";
 import { NotFound } from "../../components/NotFound/NotFound";
+import useMediaQuery from "../../utils/hooks/useMediaQuery";
 
 export const SingleNews = ({ newsData }) => {
   let { id } = useParams();
 
   const news = newsData.find((item) => item.id == id);
+  const isMobile = useMediaQuery("(max-width: 500px)");
 
   return (
     <>
@@ -16,10 +18,18 @@ export const SingleNews = ({ newsData }) => {
         {news ? (
           <div className="singleNews__card" key={news.id}>
             <div className="singleNews__text-block">
-              <p className="singleNews__create-date">
-              {news.date ? news.date.slice(0, 10).replace(/-/g, ".") : ''}
-              </p>
-              <h3 className="singleNews__title">{news.name}</h3>
+              {!isMobile ? (
+                <>
+                  {" "}
+                  <p className="singleNews__create-date">
+                    {news.date ? news.date.slice(0, 10).replace(/-/g, ".") : ""}
+                  </p>
+                  <h1 className="singleNews__title">{news.name}</h1>{" "}
+                </>
+              ) : (
+                ""
+              )}
+
               <p className="singleNews__text">{news.description}</p>
               {news.video ? (
                 <iframe
@@ -39,7 +49,14 @@ export const SingleNews = ({ newsData }) => {
                 <img className="singleNews__img" alt="" src={plugNews} />
               ) : (
                 news.images.map((el) => (
-                  <img className="singleNews__img" alt="" src={`http://135.181.198.180:8080${el.image.slice(22, 2000)}`} />
+                  <img
+                    className="singleNews__img"
+                    alt=""
+                    src={`http://135.181.198.180:8080${el.image.slice(
+                      22,
+                      2000
+                    )}`}
+                  />
                 ))
               )}
               <img className="singleNews__img" alt="" src={news.images} />
