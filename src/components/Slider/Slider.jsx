6a6aff4ec82ffc39cf13020cb16667bc88d4be65
Ruslan Gaziev <grinrus@yandex.ru>
2022-12-20 01:27:25 +0300

    Slider infinity was done

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,7 @@
 import "./Slider.sass";
 import { useEffect, useState } from "react";
 import { changeUrl } from "../../utils/changeUrl";
-import { getPlug } from '../../utils/getPlug';
+import { getPlug } from "../../utils/getPlug";
 
 const Slider = ({ data, isOpened }) => {
   const { images, name } = data;
@@ -9,34 +9,41 @@ const Slider = ({ data, isOpened }) => {
   const [img, setImg] = useState();
 
   // Вычисляем индекс предыдущего слайда
-  const prevImgIndex = activeIndex ? activeIndex - 1 : img?.length - 1;
+  const prevImgIndex = activeIndex === 0 ? img?.length - 1 : activeIndex - 1;
   // Вычисляем индекс следующего слайда
   const nextImgIndex = activeIndex === img?.length - 1 ? 0 : activeIndex + 1;
 
+  console.log(prevImgIndex, activeIndex, nextImgIndex);
+
   const handleClickNext = () => {
-    if (activeIndex >= img.length - 1) {
-      return;
+    if (activeIndex === img?.length - 1) {
+      setActiveIndex(0);
+    } else {
+      setActiveIndex(activeIndex + 1);
     }
-    setActiveIndex(activeIndex + 1);
   };
 
   const handleClickPrev = () => {
     if (activeIndex === 0) {
-      return;
+      setActiveIndex(img?.length - 1);
+    } else {
+      setActiveIndex(activeIndex - 1);
     }
-    setActiveIndex(activeIndex - 1);
   };
 
   useEffect(() => {
     // images && setImg(changeUrl(images));
-    if(!images || images?.length === 0) { setImg(getPlug('news')) } else { setImg(images) };
-
+    if (!images || images?.length === 0) {
+      setImg(getPlug());
+    } else {
+      setImg(images);
+    }
   }, [images]);
 
   // Обнуление индекса картинки при повторном открытии слайдера
   useEffect(() => {
     if (isOpened === false) setActiveIndex(0);
-  }, [isOpened])
+  }, [isOpened]);
 
   return (
     <div className="slider">
@@ -45,13 +52,25 @@ const Slider = ({ data, isOpened }) => {
           <button className="slider__prev-img-btn" onClick={handleClickPrev} />
           <div className="slider-container">
             <div className="slider-img slider-img-prev" key={prevImgIndex}>
-            <img className="slider__single-img" src={img[prevImgIndex]?.image} alt={img[prevImgIndex]?.news} />
+              <img
+                className="slider__single-img"
+                src={img[prevImgIndex]?.image}
+                alt={img[prevImgIndex]?.news}
+              />
             </div>
             <div className="slider-img" key={activeIndex}>
-              <img className="slider__single-img" src={img[activeIndex]?.image} alt={img[activeIndex]?.news} />
+              <img
+                className="slider__single-img"
+                src={img[activeIndex]?.image}
+                alt={img[activeIndex]?.news}
+              />
             </div>
             <div className="slider-img slider-img-next" key={nextImgIndex}>
-            <img className="slider__single-img" src={img[nextImgIndex]?.image} alt={img[nextImgIndex]?.news} />
+              <img
+                className="slider__single-img"
+                src={img[nextImgIndex]?.image}
+                alt={img[nextImgIndex]?.news}
+              />
             </div>
           </div>
           <button className="slider__next-img-btn" onClick={handleClickNext} />
