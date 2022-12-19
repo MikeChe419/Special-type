a6a576dd5d7457b2e1659702c07854caaf8bf6ae
Vlad <belousov7vlad@gmail.com>
2022-12-20 00:32:43 +0300

    data fix

--- src/TEMP_NEWS.js
+++ src/TEMP_NEWS.js
@@ -34,7 +34,6 @@ const tempNews = [
     date: "10-10-2022T23:02:11+03:00",
     name: "Елена Романова2",
     description: "Оля,огромное вам спасибо! Для Ромы это очень ВАЖНО!...",
-    images: [],
     id: 3,
   },
   {
@@ -62,14 +61,12 @@ const tempNews = [
     date: "10-10-2022T23:02:11+03:00",
     name: "Елена Романова2",
     description: "Оля,огромное вам спасибо! Для Ромы это очень ВАЖНО!...",
-    images: [],
     id: 6,
   },
   {
     date: "10-10-2022T22:02:11+03:00",
     name: "Елена Романова1",
     description: "Оля,огромное вам спасибо! Для Ромы это очень ВАЖНО!...",
-    images: [  ],
     id: 7,
   },
   {
@@ -89,15 +86,12 @@ const tempNews = [
     date: "10-10-2022T23:02:11+03:00",
     name: "Елена Романова2",
     description: "Оля,огромное вам спасибо! Для Ромы это очень ВАЖНО!...",
-    images: [],
     id: 9,
   },
   {
     date: "10-10-2022T22:02:11+03:00",
     name: "Елена Романова1",
     description: "Оля,огромное вам спасибо! Для Ромы это очень ВАЖНО!...",
-    images: [
-    ],
     id: 10,
   },
   {
@@ -106,8 +100,6 @@ const tempNews = [
       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
       description:
       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    images: [
-    ],
     id: 11,
   }
 ];
