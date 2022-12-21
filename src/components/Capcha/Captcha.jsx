53832ea0daf3fbb5169238a0623df0187103faf0
Ruslan Gaziev <grinrus@yandex.ru>
2022-12-22 02:00:14 +0300

    Comments captcha element were added

--- src/components/Capcha/Captcha.jsx
+++ src/components/Capcha/Captcha.jsx
@@ -2,32 +2,40 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { mainApi } from "../../utils/api/mainApi";
 import { useLocation } from 'react-router-dom';
+import { useRef } from 'react';
 import "./Captcha.sass";
 
 const Captcha = () => {
   const [captcha, setCaptcha] = useState({});
   const {pathname} = useLocation();
   const [style, setStyle] = useState({});
-  console.log(pathname)
+  const [captchaValue, setCaptchaValue] = useState(null)
 
+  // Функция загрузки капчи с сервера
   const getCaptcha = () =>
     mainApi
       .postCaptcha()
       .then((res) => setCaptcha(res))
       .catch((err) => console.log(err));
 
+  // Получаем значение input с введенными пользователем данными
+  const handleChange = (e) => {
+    setCaptchaValue(e.target.value)
+  }
+
+  // Запускаем функцию запроса капчи при загрузке элемента
   useEffect(() => {
     getCaptcha();
   }, []);
 
+  // Проверяем адрес страницы на которой отрисовывается элемент и меняем стиль
+  // Если страница с формой Помощь, то макс ширина компонента 100%
   useEffect(() => {
     if (!pathname) return;
     if (pathname === '/help/volunteering') setStyle({
       maxWidth: '100%'
     })
   }, [pathname])
-
-  console.log(captcha);
 
   return (
     <div className="captcha" style={style}>
@@ -38,13 +46,19 @@ const Captcha = () => {
             backgroundImage: `url('data:image/png;base64,${captcha?.captcha_image}')`,
           }}
         />
-        <button className="captcha__btn" onClick={() => getCaptcha()}>
+        <button type='button' className="captcha__btn" onClick={() => getCaptcha()}>
           Обновить <div className='captcha__btn-icon'/>
         </button>
       </div>
       <input
+        type='text'
+        name='captchaValue'
         className="captcha__input"
         placeholder="Введите код с картинки"
+        onChange={handleChange}
+        required
+        maxLength='4'
+        minLength='4'
       />
     </div>
   );
