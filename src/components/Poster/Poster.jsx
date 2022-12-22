a3a008449419a8f2cb093d3d2c18d02590ba93d9
Vlad B <belousov7vlad@gmail.com>
2022-12-22 13:23:46 +0300

    poster start data

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -16,8 +16,8 @@ const Poster = ({ postersData, setItemForRegistration }) => {
           <li className="poster" key={el.id} onClick={() => handleOnClick(el)}>
             <ImageMasked item={el} lable="poster" />
             <h3 className="poster__title">{el.name}</h3>
-            <h5 className="poster__subtitle">Начало:</h5>
-            <span className="poster__span">{el.startTime}</span>
+            <h5 className="poster__subtitle">Начало: </h5> 
+            <span className="poster__span">{el.date ? el.date.slice(11, 16).replace(/-/g, ".") : ''}</span>
             <h5 className="poster__subtitle">Цена билета:</h5>
             <span className="poster__span_red">{`${el.price}р`}</span>
             <h5 className="poster__subtitle">Адрес проведения:</h5>
