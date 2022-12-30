578cf2df1a95785f8d32f29af4891bc8f643e952
Vlad B <belousov7vlad@gmail.com>
2022-12-31 00:16:17 +0300

    regex fix

--- src/utils/regExp.js
+++ src/utils/regExp.js
@@ -3,7 +3,9 @@ export const nameRegExp = /^[a-zа-яё-]+$/iu;
 // Для валидации поля, которое содержит имя и фамилию одновременно
 export const fullNameRegExp = /^[a-zа-яё ,.'-]+$/iu;
 
-export const descriptionRegExp = /^[a-z0-9а-яё; .,-]+$/iu;
+// export const descriptionRegExp = /^[a-z0-9а-яё; .,-]+$/iu;
+
+export const descriptionRegExp = /^[a-z0-9а-яё;.,-]+[ ][a-z0-9а-яё;.,-]+$/iu;
 
 export const emailRegExp = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 
